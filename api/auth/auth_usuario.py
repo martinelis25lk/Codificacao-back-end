0e6f0b85a8b9444474be28a8cd0a2d25e939df51
martinelis25lk <gui.almeida.edu@gmail.com>
2024-11-05 17:39:43 -0300

    criação da rota de autenticação de login

--- api/auth/auth_usuario.py
+++ api/auth/auth_usuario.py
@@ -9,6 +9,7 @@ from jose import jwt, JWTError
 from decouple import config
 from models.usuario_model import UserModel
 from auth.schemas import User
+from jose import jwt, JWTError
 
 
 SECRET_KEY = config('SECRET_KEY')
@@ -22,7 +23,7 @@ class UserUseCases:
         self.db_session = db_session
 
 
-    def user_register(self, user: User):#user do tipo schema
+    def usuario_register(self, user: User):#user do tipo schema
         user_model = UserModel(
             username=user.username,
             password=crypt_context.hash(user.password)
@@ -35,3 +36,30 @@ class UserUseCases:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail='Usuario já existe'
             )
+        
+    def usuario_login(self, user: User, expira_em : int = 30):
+        usuario_no_bd = self.db_session.query(UserModel).filter_by(username = user.username).first()
+
+        if usuario_no_bd is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Nome de usuário ou Senha incorretos"
+            )
+        
+        if not crypt_context.verify(user.password, usuario_no_bd.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Nome de usuário ou Senha incorretos"
+            )
+        
+        exp = datetime.utcnow() + timedelta(minutes=expira_em)
+
+        payload ={
+            'sub': user.username,
+            'exp': exp
+        }
+
+        token_de_acesso = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+        return {'acess_token': token_de_acesso,
+                'expiração'  : exp.isoformat()}
--- api/routers/usuario_router.py
+++ api/routers/usuario_router.py
@@ -4,13 +4,9 @@ from sqlalchemy.orm import Session
 from shared.dependencies import get_db
 from auth.auth_usuario import UserUseCases
 from auth.schemas import User
-
-
-
-
+from fastapi.security import OAuth2PasswordRequestForm
 
 #from app.depends import get_db_session, token_verifier
-
 
 router = APIRouter(prefix='/usuario')
 
@@ -21,7 +17,7 @@ def user_register(
     db_session: Session = Depends(get_db),
 ):
     uc = UserUseCases(db_session=db_session)
-    uc.user_register(user=user)
+    uc.usuario_register(user=user)
     return JSONResponse(
         content={'msg': 'success'},
         status_code=status.HTTP_201_CREATED,
@@ -29,3 +25,18 @@ def user_register(
 
 
 
+@router.post('/login')
+def user_register(login_request_form : OAuth2PasswordRequestForm= Depends()
+                  ,db_session: Session = Depends(get_db)):
+    
+    uc = UserUseCases(db_session=db_session)
+    user = User(
+        username=login_request_form.username,
+        password=login_request_form.password
+    )
+
+    auth_data = uc.usuario_login(user=user)
+    return JSONResponse(
+        content=auth_data,
+        status_code=status.HTTP_200_OK
+    )
